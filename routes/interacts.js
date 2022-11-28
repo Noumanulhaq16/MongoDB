a49c191e21d1ab0e8f8c75d53ccb3055f4d522e8
noumaulhaq16 <noumanulhaq16@gmail.com>
2022-11-28 16:53:05 +0500

    first

---
+++ routes/interacts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const interactsController = require('../controllers/interact');
+const auth = require("../middleware/auth");
+
+router.get('/getinteracts', auth, interactsController.getInteract);
+router.post('/likePartner/:id', auth, interactsController.likePartner);
+router.post('/dislikePartner/:id', auth, interactsController.dislikePartner);
+router.get('/getRooms', auth, interactsController.getRooms);
+
+
+module.exports = router;
